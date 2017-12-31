d1ecd711699cf00531faa7bbc128c938779ebe50
Dmitry Kunin <dkunin@avito.ru>
2017-12-31 21:44:10 +0300

    Minor optimizations

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const cardFront = document.querySelector('.card-front');
 const cardBack = document.querySelector('.card-back');
 const player = document.querySelector('.player');
 const noSound = document.querySelector('.no-sound');
+const clickMeLink = document.querySelector('.click-me');
 
 cards.forEach(singleCard => {
     singleCard.addEventListener('click', function() {
         player.play();
+        clickMeLink.setAttribute('hidden', true);
         cardFront.classList.toggle('card-flip');
         cardBack.classList.toggle('card-flip');
         noSound.classList.add('playing');
